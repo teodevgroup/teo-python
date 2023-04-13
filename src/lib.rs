3b45eb1277135ce69e2c208b3122980ce5648c39
Victor Teo <victor.teo@fillmula.com>
2023-04-13 22:43:26 +0800

    fix compile

--- src/lib.rs
+++ src/lib.rs
@@ -65,8 +65,9 @@ impl App {
                 check_callable(callback)?;
                 let py_object = teo_value_to_py_object(value, py)?;
                 let transformed_py = callback.call1((py_object,))?;
-                let transformed_py_awaited = await_coroutine_if_needed(transformed_py, py)?.as_ref(py);
-                let transformed = py_object_to_teo_value(transformed_py_awaited, py)?;
+                let transformed_py_awaited = await_coroutine_if_needed(transformed_py, py)?;
+                let transformed_py_awaited_ref = transformed_py_awaited.as_ref(py);
+                let transformed = py_object_to_teo_value(transformed_py_awaited_ref, py)?;
                 Ok(transformed)
             }).into_teo_result()
         });
--- src/utils/await_coroutine_if_needed.rs
+++ src/utils/await_coroutine_if_needed.rs
@@ -1,9 +1,9 @@
-use pyo3::{IntoPy, PyAny, PyObject, Python};
+use pyo3::{IntoPy, PyAny, PyObject, PyResult, Python};
 use pyo3_asyncio::{into_future_with_locals, TaskLocals};
 use crate::utils::is_coroutine::is_coroutine;
 
-pub fn await_coroutine_if_needed(transformed_py: &PyAny, py: Python<'_>) -> PyObject {
-    let transformed_py = if is_coroutine(transformed_py, py)? {
+pub fn await_coroutine_if_needed(transformed_py: &PyAny, py: Python<'_>) -> PyResult<PyObject> {
+    Ok(if is_coroutine(transformed_py, py)? {
         let asyncio = py.import("asyncio")?;
         let event_loop = asyncio.call_method0("get_event_loop").unwrap_or(
             {
@@ -18,5 +18,5 @@ pub fn await_coroutine_if_needed(transformed_py: &PyAny, py: Python<'_>) -> PyOb
         })?
     } else {
         transformed_py.into_py(py)
-    };
+    })
 }
