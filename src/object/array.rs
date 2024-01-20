ba11a66e52f3f5882297bdde7f1ea7bdcb1f4bd9
Victor Teo <victor.teo@fillmula.com>
2024-01-20 14:07:15 +0800

    py any to teo object

---
+++ src/object/array.rs
@@ -0,0 +1,12 @@
+use pyo3::{Python, PyResult, PyObject, types::PyList, IntoPy};
+use teo::prelude::object::Object;
+use crate::object::teo_object_to_py_any;
+
+pub fn teo_array_to_py_any(py: Python<'_>, array: &Vec<Object>) -> PyResult<PyObject> {
+    let mut list = PyList::empty(py);
+    for (i, value) in array.iter().enumerate() {
+        let v = teo_object_to_py_any(py, value)?;
+        list.append(v);
+    }
+    Ok(list.into_py(py))
+}
