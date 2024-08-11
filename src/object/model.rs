7ecd7cf8feff1f07b952397ad494a561573272dc
Victor Teo <victor.teo@fillmula.com>
2024-08-11 22:07:15 +0800

    update

--- src/object/model.rs
+++ src/object/model.rs
@@ -1,8 +1,9 @@
 use pyo3::{Python, PyResult, PyObject};
-use teo::prelude::model;
+use teo::prelude::{app::data::AppData, model};
 
-use crate::dynamic::py_model_object_from_teo_model_object;
+use crate::dynamic::py_class_lookup_map::PYClassLookupMap;
 
-pub fn teo_model_object_to_py_any(py: Python<'_>, model_object: &model::Object) -> PyResult<PyObject> {
-    py_model_object_from_teo_model_object(py, model_object.clone())
+pub fn teo_model_object_to_py_any(py: Python<'_>, model_object: &model::Object, app_data: &AppData) -> PyResult<PyObject> {
+    let map = PYClassLookupMap::from_app_data(app_data);
+    map.teo_model_object_to_py_model_object_object(py, model_object.clone())
 }
