3a1e7ae8ec3c9ffc4fd7dd6c59d8af7bf28a57cc
Victor Teo <victor.teo@fillmula.com>
2024-11-18 14:21:47 +0800

    try cast

--- src/handler/group.rs
+++ src/handler/group.rs
@@ -1,3 +1,4 @@
+use pyo3::types::PyCapsule;
 use pyo3::{pyclass, pymethods, Bound, Py, PyAny, PyErr, PyObject, PyResult, Python};
 use teo::prelude::handler;
 use teo::prelude::request::Request as TeoRequest;
@@ -16,21 +17,24 @@ impl HandlerGroup {
 
     pub fn define_handler(&mut self, py: Python<'_>, name: String, callback: Bound<PyAny>) -> PyResult<()> {
         check_callable(&callback)?;
+        let callback_object = Py::from(callback);
+        let callback_object_never_ends = &*Box::leak(Box::new(callback_object));
         let main_thread_locals = &*Box::leak(Box::new(pyo3_async_runtimes::tokio::get_current_locals(py)?));
-        let callback_owned = &*Box::leak(Box::new(Py::from(callback)));
-        self.teo_handler_group.define_handler(name.as_str(), move |request: TeoRequest| async move {
-            let result = Python::with_gil(|py| {
-                let request = Request {
-                    teo_request: request
-                };
-                let result = callback_owned.call1(py, (request,))?;
-                Ok::<PyObject, PyErr>(result)
-            })?;
-            let awaited_result = await_coroutine_if_needed_value_with_locals(&result, main_thread_locals).await?;
-            Python::with_gil(|py| {
-                let response: Response = awaited_result.extract(py)?;
-                Ok(response.teo_response.clone())
-            })
+        self.teo_handler_group.define_handler(name.as_str(), move |request: TeoRequest| {
+            async move {
+                let result = Python::with_gil(|py| {
+                    let request = Request {
+                        teo_request: request
+                    };
+                    let result = callback_object_never_ends.call1(py, (request,))?;
+                    Ok::<PyObject, PyErr>(result)
+                })?;
+                let awaited_result = await_coroutine_if_needed_value_with_locals(&result, main_thread_locals).await?;
+                Python::with_gil(|py| {
+                    let response: Response = awaited_result.extract(py)?;
+                    Ok(response.teo_response.clone())
+                })
+            }
         });
         Ok(())
     }
