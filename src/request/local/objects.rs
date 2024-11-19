f1b54357467874d96d4a42f7a1fbe9bc25170397
Victor Teo <victor.teo@fillmula.com>
2024-11-19 16:09:09 +0800

    local methods

---
+++ src/request/local/objects.rs
@@ -0,0 +1,41 @@
+use pyo3::{pyclass, pymethods, Bound, IntoPy, PyAny, PyObject, PyResult, Python};
+use teo::prelude::request::local_objects::LocalObjects as TeoLocalObjects;
+
+#[pyclass]
+pub struct LocalObjects {
+    pub(crate) teo_local_objects: TeoLocalObjects,
+}
+
+#[pymethods]
+impl LocalObjects {
+
+    pub fn insert(&self, key: String, value: Bound<PyAny>, py: Python<'_>) -> PyResult<()> {
+        let value = value.into_py(py);
+        self.teo_local_objects.insert(key, value);
+        Ok(())
+    }
+
+    pub fn get(&self, key: String, py: Python<'_>) -> PyResult<PyObject> {
+        let value: Option<&PyObject> = self.teo_local_objects.get(&key);
+        match value {
+            Some(value) => {
+                Ok(value.clone_ref(py))
+            },
+            None => {
+                Ok(py.None())
+            }
+        }
+    }
+
+    pub fn contains(&self, key: String) -> bool {
+        self.teo_local_objects.contains(&key)
+    }
+
+    pub fn remove(&self, key: String) {
+        self.teo_local_objects.remove(key.as_str());
+    }
+
+    pub fn clear(&self) {
+        self.teo_local_objects.clear();
+    }
+}
---
+++ src/request/local/values.rs
@@ -0,0 +1,36 @@
+use pyo3::{pyclass, pymethods, Bound, PyAny, PyObject, PyResult, Python};
+use teo::prelude::{request::local_values::LocalValues as TeoLocalValues, Value};
+
+use crate::object::value::{py_any_to_teo_value, teo_value_to_py_any_without_model_objects};
+
+#[pyclass]
+pub struct LocalValues {
+    pub(crate) teo_local_values: TeoLocalValues,
+}
+
+#[pymethods]
+impl LocalValues {
+
+    pub fn insert(&self, key: String, value: Bound<PyAny>, py: Python<'_>) -> PyResult<()> {
+        let value = py_any_to_teo_value(py, &value)?;
+        self.teo_local_values.insert(key, value);
+        Ok(())
+    }
+
+    pub fn get(&self, key: String, py: Python<'_>) -> PyResult<PyObject> {
+        let value: &Value = self.teo_local_values.get(&key)?;
+        Ok(teo_value_to_py_any_without_model_objects(py, value)?)
+    }
+
+    pub fn contains(&self, key: String) -> bool {
+        self.teo_local_values.contains(&key)
+    }
+
+    pub fn remove(&self, key: String) {
+        self.teo_local_values.remove(key.as_str());
+    }
+
+    pub fn clear(&self) {
+        self.teo_local_values.clear();
+    }
+}
