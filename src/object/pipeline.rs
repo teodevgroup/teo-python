a259c2ba45a6fc4c5e79612dee4e5cd58802f477
Victor Teo <victor.teo@fillmula.com>
2024-12-03 19:56:38 +0800

    update pipeline

--- src/object/pipeline.rs
+++ src/object/pipeline.rs
@@ -1,12 +1,28 @@
 
-use pyo3::{pyclass, IntoPyObjectExt, PyObject, PyResult, Python};
-use teo::prelude::Pipeline as TeoPipeline;
+use pyo3::{pyclass, pymethods, IntoPyObjectExt, PyObject, PyResult, Python};
+use teo::prelude::Pipeline as OriginalPipeline;
 
 #[pyclass]
 pub struct Pipeline {
-    pub(crate) value: TeoPipeline
+    pub(crate) original: OriginalPipeline
 }
 
-pub fn teo_pipeline_to_py_any<'p>(py: Python<'p>, pipeline: &TeoPipeline) -> PyResult<PyObject> {
-    Ok(Pipeline { value: pipeline.clone() }.into_py_any(py)?)
+#[pymethods]
+impl Pipeline {
+
+    pub fn __len__(&self) -> usize {
+        self.original.len()
+    }
+
+    pub fn __str__(&self) -> String {
+        format!("{}", self.original)
+    }
+
+    pub fn __repr__(&self) -> String {
+        format!("{}", self.original)
+    }
+}
+
+pub fn teo_pipeline_to_py_any<'p>(py: Python<'p>, pipeline: &OriginalPipeline) -> PyResult<PyObject> {
+    Ok(Pipeline { original: pipeline.clone() }.into_py_any(py)?)
 }
