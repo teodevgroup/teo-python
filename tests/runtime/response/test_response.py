dcf749b18991b12efd776bcb1a931bda227df4ab
Victor Teo <victor.teo@fillmula.com>
2024-12-08 15:59:52 +0800

    update tests

--- tests/runtime/response/test_response.py
+++ tests/runtime/response/test_response.py
@@ -27,14 +27,14 @@ class TestResponse(TestCase):
     async def test_cookie_in_text_response(self):
         test_request = TestRequest(method="GET", uri='/textResponse')
         response = await self.server.process(test_request)
-        self.assertEqual(response.header_value('set-cookie'), 'foo=bar')
+        self.assertEqual(response.headers['set-cookie'], 'foo=bar')
 
     async def test_cookie_in_json_response(self):
         test_request = TestRequest(method="GET", uri='/jsonResponse')
         response = await self.server.process(test_request)
-        self.assertEqual(response.header_value('set-cookie'), 'foo=bar')
+        self.assertEqual(response.headers['set-cookie'], 'foo=bar')
 
     async def test_cookie_in_file_response(self):
         test_request = TestRequest(method="GET", uri='/fileResponse')
         response = await self.server.process(test_request)
-        self.assertEqual(response.header_value('set-cookie'), 'foo=bar')
+        self.assertEqual(response.headers['set-cookie'], 'foo=bar')
