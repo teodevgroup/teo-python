5638a7fa997347661fa2b633812663f46e4f7772
Victor Teo <victor.teo@fillmula.com>
2024-12-14 12:37:48 +0800

    remove annotation mark for typed dict

--- teo/annotations/__init__.py
+++ teo/annotations/__init__.py
@@ -1,14 +1,3 @@
-from typing import TypedDict
-
-
-class CapturesAnnotationMark(TypedDict):
-    pass
-
-
-class RequestBodyObjectAnnotationMark(TypedDict):
-    pass
-
-
 class TeoAnnotationMark:
     pass
 
